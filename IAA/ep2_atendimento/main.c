85aae0a48fbc539627a7eb139b8e5b55d82f3177
Douglasm14 <macedodouglas52@gmail.com>
2025-11-20 02:00:09 -0300

    refactor? ep iaa 2

--- IAA/ep2_atendimento/main.c
+++ IAA/ep2_atendimento/main.c
@@ -2,15 +2,17 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define MAX_STRING 32
 #define MAX_LINE 128
 #define NUM_PRIORITIES 5
 #define NUM_CATEGORIES 2
 
+const char *PRIORITIES[] = {"Vermelho", "Laranja", "Amarelo", "Verde", "Azul"};
+const int TIME_LIMITS[] = {0, 10, 60, 120, 240}; // Limites para reclassificação
+
 typedef struct
 {
-    char priority[MAX_STRING];
-    char category[MAX_STRING];
+    char priority[32];
+    char category[32];
     int priorityID;
     int categoryID;
     int waitTime;
@@ -19,20 +21,26 @@ typedef struct
 typedef struct
 {
     int size;
+    int maxWaitTime;
     Patient *patients;
 } Data;
+
 
 // Funcoes que acham o id compativel de acordo com a string
 int priorityID(char *string);
 int categoryID(char *string);
 // Funcao que reclassifica a propridade do paciente de acordo com o quanto tempo ele esta esperando
-void reclassify(Data *data, int i, int priority, int time);
+void reclassify(Patient *p);
+// Funcao que le o arquivo e escreve ele em uma variavel
+int readData(Data *data, char *file);
 // Funcao que ordena todos os dados pela maior tempo de espera
 int countingSortTime(Data *data, int size);
-// Funcao que ordena todos os dados pela categoria do menor id para o maior
+// Funcao que ordena os dados que tem priorityID maior que 1 pelas categorias de menor id para a maior
 int countingSortCategory(Data *data, int size);
 // Funcao que ordena todos os dados pela prioridade do menor id para o maior
 int countingSortPriority(Data *data, int size);
+// Funcao que escreve os dados ordenados em um aquivo csv
+int writeSortedData(Data data, char *string);
 
 int main(int argc, char *argv[])
 {
@@ -41,63 +49,12 @@ int main(int argc, char *argv[])
         return 1;
     }
 
-    FILE *data_csv;
-    data_csv = fopen(argv[1], "r");
-
-    if (!data_csv) {
-        perror("Falha ao ler o arquivo\n");
-        return 1;
-    }
-
     Data data;
-    data.size = 0;
 
-    char bufferLine[MAX_LINE];
-    char *token;
-    int maxWaitTime = 1;
-
-    while (fgets(bufferLine, sizeof(bufferLine), data_csv))
-        data.size++;
-
-    data.patients = (Patient *)malloc(data.size * sizeof(Patient));
-
-    if (!data.patients) {
-        perror("Erro ao alocar memoria");
-        fclose(data_csv);
+    if (readData(&data, argv[1]))
         return 1;
-    }
 
-    rewind(data_csv);
-
-    for (int i = 0; (fgets(bufferLine, sizeof(bufferLine), data_csv)); i++) {
-        bufferLine[strcspn(bufferLine, "\n")] = 0;
-
-        token = strtok(bufferLine, ",");
-        if (token != NULL) {
-            strcpy(data.patients[i].priority, token);
-            data.patients[i].priorityID = priorityID(data.patients[i].priority);
-        }
-
-        token = strtok(NULL, ",");
-        if (token != NULL) {
-            strcpy(data.patients[i].category, token);
-            data.patients[i].categoryID = categoryID(data.patients[i].category);
-        }
-
-        token = strtok(NULL, ",");
-        if (token != NULL) {
-            data.patients[i].waitTime = atoi(token);
-            if (maxWaitTime < data.patients[i].waitTime) 
-                maxWaitTime = data.patients[i].waitTime;
-        }
-
-        if (data.patients[i].priorityID != 0)
-            reclassify(&data, i, data.patients[i].priorityID, data.patients[i].waitTime);
-    }
-
-    fclose(data_csv);
-
-    if (countingSortTime(&data, maxWaitTime))
+    if (countingSortTime(&data, data.maxWaitTime))
         return 1;
 
     if (countingSortCategory(&data, NUM_CATEGORIES))
@@ -106,38 +63,19 @@ int main(int argc, char *argv[])
     if (countingSortPriority(&data, NUM_PRIORITIES))
         return 1;
 
-    FILE *OrdemDeAtendimentos;
-    OrdemDeAtendimentos = fopen("OrdemDeAtendimentos.csv", "w");
-
-    if (!OrdemDeAtendimentos) {
-        perror("Falha ao escrever o arquivo\n");
+    if (writeSortedData(data, "OrdemDeAtendimentos.csv")) 
         return 1;
-    }
 
-    for (int i = 0; i < data.size; i++) {
-        fprintf(OrdemDeAtendimentos, "%s,%s,%d\n",
-                data.patients[i].priority,
-                data.patients[i].category,
-                data.patients[i].waitTime);
-    }
-
-    fclose(OrdemDeAtendimentos);
     free(data.patients);
     return 0;
 }
 
 int priorityID(char *string)
 {
-    if (strcmp(string, "Vermelho") == 0)
-        return 0;
-    if (strcmp(string, "Laranja") == 0)
-        return 1;
-    if (strcmp(string, "Amarelo") == 0)
-        return 2;
-    if (strcmp(string, "Verde") == 0)
-        return 3;
-    if (strcmp(string, "Azul") == 0)
-        return 4;
+    for (int i = 0; i < NUM_PRIORITIES; i++) {
+        if (strcmp(string, PRIORITIES[i]) == 0)
+            return i;
+    }
     return -1;
 }
 
@@ -148,55 +86,82 @@ int categoryID(char *string)
     return 0;
 }
 
-void reclassify(Data *data, int i, int priority, int time)
+void reclassify(Patient *p)
 {
-    switch (priority) {
-    case 1:
-        if (time > 10) {
-            strcpy(data->patients[i].priority, "Vermelho");
-            data->patients[i].priorityID = 0;
-        }
-        break;
-    case 2:
-        if (time > 60) {
-            strcpy(data->patients[i].priority, "Laranja");
-            data->patients[i].priorityID = 1;
-        }
-        break;
-    case 3:
-        if (time > 120) {
-            strcpy(data->patients[i].priority, "Amarelo");
-            data->patients[i].priorityID = 2;
-        }
-        break;
-    case 4:
-        if (time > 240) {
-            strcpy(data->patients[i].priority, "Verde");
-            data->patients[i].priorityID = 3;
-        }
-        break;
-    default:
-        break;
+    if(p->waitTime > TIME_LIMITS[p->priorityID]) {
+        p->priorityID--;
+        strcpy(p->priority, PRIORITIES[p->priorityID]);
     }
 }
 
-int countingSortTime(Data *data, int size)
+int readData(Data *data, char *file) {
+    FILE *data_csv;
+    data_csv = fopen(file, "r");
+
+    if (!data_csv) {
+        perror("Falha ao ler o arquivo\n");
+        return 1;
+    }
+    
+    char bufferLine[MAX_LINE];
+    
+    data->size = 0;
+    data->maxWaitTime = 0;
+
+    while (fgets(bufferLine, sizeof(bufferLine), data_csv))
+        data->size++;
+
+    data->patients = (Patient *)malloc(data->size * sizeof(Patient));
+
+    if (!data->patients) {
+        perror("Erro ao alocar memoria");
+        fclose(data_csv);
+        return 1;
+    }
+
+    rewind(data_csv);
+
+    for (int i = 0; (fgets(bufferLine, sizeof(bufferLine), data_csv)); i++) {
+        bufferLine[strcspn(bufferLine, "\n")] = 0;
+
+        char *p = strtok(bufferLine, ",");
+        char *c = strtok(NULL, ",");
+        char *t = strtok(NULL, ",");
+
+        if (p && c && t) {
+            strcpy(data->patients[i].priority, p);
+            strcpy(data->patients[i].category, c);
+            data->patients[i].waitTime = atoi(t);
+            
+            data->patients[i].priorityID = priorityID(data->patients[i].priority);
+            data->patients[i].categoryID = categoryID(data->patients[i].category);
+
+            if (data->maxWaitTime < data->patients[i].waitTime) 
+                data->maxWaitTime = data->patients[i].waitTime;
+
+            if (data->patients[i].priorityID > 0)
+                reclassify(&data->patients[i]);
+        }
+    }
+
+    fclose(data_csv);
+    return 0;
+}
+
+int countingSortTime(Data *data, int maxWaitTime)
 {
-    size++;
-    int *count = (int *)malloc(size * sizeof(int));
+    maxWaitTime++;
+    int *count = (int *)calloc(maxWaitTime, sizeof(int));
 
     if (!count) {
         perror("Erro ao alocar memoria");
         return 1;
     }
 
-    for (int i = 0; i < size; i++)
-        count[i] = 0;
-
     for (int i = 0; i < data->size; i++)
         count[data->patients[i].waitTime]++;
 
-    for (int i = size - 2; i >= 0; i--)
+    for (int i = maxWaitTime - 2; i >= 0; i--)
         count[i] = count[i] + count[i + 1];
 
     Patient *sortedData = (Patient *)malloc(data->size * sizeof(Patient));
@@ -218,102 +183,88 @@ int countingSortTime(Data *data, int size)
     return 0;
 }
 
-int countingSortCategory(Data *data, int size)
+int countingSortCategory(Data *data, int quantCategories)
 {
     Patient *selectData = (Patient *)malloc(data->size * sizeof(Patient));
+    int *idx = (int *)malloc(data->size * sizeof(int));
 
-    int *indicesSelectData = (int *)malloc(data->size * sizeof(int));
-
-    int sizeSelectData = 0;
-
-    if (!selectData || !indicesSelectData) {
+    if (!selectData || !idx) {
         perror("Erro ao alocar memoria para extracao");
         free(selectData);
-        free(indicesSelectData);
+        free(idx);
         return 1;
     }
 
+    int subSize = 0;
+
     for (int i = 0; i < data->size; i++) {
-        if (data->patients[i].priorityID != 0 && data->patients[i].priorityID != 1) {
-            selectData[sizeSelectData] = data->patients[i];
-            indicesSelectData[sizeSelectData] = i;
-            sizeSelectData++;
+        if (data->patients[i].priorityID > 1) {
+            selectData[subSize] = data->patients[i];
+            idx[subSize] = i;
+            subSize++;
         }
     }
 
-    if (sizeSelectData == 0) {
-        free(selectData);
-        free(indicesSelectData);
-        return 0;
-    }
+    if (subSize > 0) {
+        int *count = (int *)calloc(quantCategories, sizeof(int));
+        Patient *sortedSubset = (Patient *)malloc(subSize * sizeof(Patient));
 
-    int *count = (int *)malloc(size * sizeof(int));
+        if (!count || !sortedSubset) {
+            perror("Erro ao alocar buffers de ordenacao'");
+            free(selectData);
+            free(idx);
+            free(count);
+            free(sortedSubset);
+            return 1;
+        }
 
-    if (!count) {
-        perror("Erro ao alocar memoria para 'count'");
-        free(selectData);
-        free(indicesSelectData);
-        return 1;
-    }
+        for (int i = 0; i < subSize; i++)
+            count[selectData[i].categoryID]++;
 
-    for (int i = 0; i < size; i++)
-        count[i] = 0;
+        for (int i = 1; i < quantCategories; i++)
+            count[i] = count[i] + count[i - 1];
 
-    for (int i = 0; i < sizeSelectData; i++)
-        count[selectData[i].categoryID]++;
+        for (int i = subSize - 1; i >= 0; i--) {
+            sortedSubset[count[selectData[i].categoryID] - 1] = selectData[i];
+            count[selectData[i].categoryID]--;
+        }
 
-    for (int i = 1; i < size; i++)
-        count[i] = count[i] + count[i - 1];
+        for (int i = 0; i < subSize; i++)
+            data->patients[idx[i]] = sortedSubset[i];
 
-    Patient *arrayJaOrdenado = (Patient *)malloc(sizeSelectData * sizeof(Patient));
-    if (!arrayJaOrdenado) {
-        perror("Erro ao alocar memoria para 'arrayJaOrdenado'");
-        free(selectData);
-        free(indicesSelectData);
+        free(sortedSubset);
         free(count);
-        return 1;
     }
-
-    for (int i = sizeSelectData - 1; i >= 0; i--) {
-        arrayJaOrdenado[count[selectData[i].categoryID] - 1] = selectData[i];
-        count[selectData[i].categoryID]--;
-    }
-
+    
     free(selectData);
-    free(count);
-
-    for (int i = 0; i < sizeSelectData; i++)
-        data->patients[indicesSelectData[i]] = arrayJaOrdenado[i];
-
-    free(indicesSelectData);
-    free(arrayJaOrdenado);
-
+    free(idx);
     return 0;
 }
 
-int countingSortPriority(Data *data, int size)
+int countingSortPriority(Data *data, int quantPriorities)
 {
-    size++;
-    int *count = (int *)malloc(size * sizeof(int));
+    quantPriorities++;
+    int *count = (int *)calloc(quantPriorities, sizeof(int));
 
     if (!count) {
         perror("Erro ao alocar memoria");
         return 1;
     }
 
-    for (int i = 0; i < size; i++)
+    for (int i = 0; i < quantPriorities; i++)
         count[i] = 0;
 
     for (int i = 0; i < data->size; i++)
         count[data->patients[i].priorityID]++;
 
-    for (int i = 1; i < size; i++)
+    for (int i = 1; i < quantPriorities; i++)
         count[i] = count[i] + count[i - 1];
 
     Patient *sortedData = (Patient *)malloc(data->size * sizeof(Patient));
 
     if (!sortedData) {
         perror("Erro ao alocar memoria");
+        free(count);
         return 1;
     }
 
@@ -325,5 +276,25 @@ int countingSortPriority(Data *data, int size)
     free(data->patients);
     data->patients = sortedData;
     free(count);
+    return 0;
+}
+
+int writeSortedData(Data data, char *string) {
+    FILE *OrdemDeAtendimentos;
+    OrdemDeAtendimentos = fopen(string, "w");
+
+    if (!OrdemDeAtendimentos) {
+        perror("Falha ao escrever o arquivo\n");
+        return 1;
+    }
+
+    for (int i = 0; i < data.size; i++) {
+        fprintf(OrdemDeAtendimentos, "%s,%s,%d\n",
+                data.patients[i].priority,
+                data.patients[i].category,
+                data.patients[i].waitTime);
+    }
+
+    fclose(OrdemDeAtendimentos);
     return 0;
 }
