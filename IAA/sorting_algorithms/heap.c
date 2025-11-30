c3d203a292d0b8046c557dabdd208e22837b1f6d
Douglas <Douglas1412@usp.br>
2025-11-30 18:25:55 -0300

    fix heapsort

--- IAA/sorting_algorithms/heap.c
+++ IAA/sorting_algorithms/heap.c
@@ -50,10 +50,10 @@ void heapify(int arr[], int n, int i) {
     int left = 2 * i + 1;
     int right = 2 * i + 2;
 
-    if (left < n && arr[left] > arr[right])
+    if (left < n && arr[left] > arr[largest])
         largest = left;
 
-    if (right < n && arr[right] > arr[left])
+    if (right < n && arr[right] > arr[largest])
         largest = right;
 
     if (largest != i) {
